be78827588309ed9ae018320f28578ea067f0ab0
Hendra Tommy Wijaya <hendra.tommy.w@gmail.com>
2020-05-29 01:08:13 +0700

    init commit

---
+++ example/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"errors"
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/template/html"
+	errhandler "github.com/hendratommy/github.com/hendratommy/fiber-errhandler"
+)
+
+var data = fiber.Map{
+	"firstName": "John",
+	"lastName":  "Doe",
+}
+
+func main() {
+	app := fiber.New()
+	app.Settings.Templates = html.New("./views", ".html")
+
+	app.Use(errhandler.New(errhandler.Config{
+		UseTemplate: true,
+		//Log: true,
+		Handler: func(c *fiber.Ctx, err error, fallback func(...interface{})) {
+			if he, ok := err.(errhandler.HTTPError); ok {
+				switch he.StatusCode() {
+				case fiber.StatusUnauthorized:
+					c.Status(he.StatusCode()).Render("custom-err", fiber.Map{
+						"StatusCode": he.StatusCode(),
+						"Message":    he.Message(),
+						"Reason":     "Please login first",
+						"SomeData":   he.Data(),
+					})
+					return
+
+				default:
+					break
+				}
+			}
+			fallback(err)
+		},
+	}))
+
+	apiv1 := app.Group("/api")
+	apiv1.Get("/", func(c *fiber.Ctx) {
+		c.JSON(data)
+	})
+	apiv1.Get("/panic", func(c *fiber.Ctx) {
+		panic(errors.New("winter is coming to the api"))
+	})
+	apiv1.Get("/err-403", func(c *fiber.Ctx) {
+		c.Next(errhandler.NewHttpError(fiber.StatusForbidden, "Cannot access this", nil))
+	})
+
+	app.Get("/", func(c *fiber.Ctx) {
+		c.Render("index", data)
+	})
+	app.Get("/panic", func(c *fiber.Ctx) {
+		panic(errors.New("winter is coming to the web"))
+	})
+	app.Get("/err-403", func(c *fiber.Ctx) {
+		c.Next(errhandler.NewHttpError(fiber.StatusForbidden, "Cannot access this", nil))
+	})
+	app.Get("/custom", func(c *fiber.Ctx) {
+		c.Next(errhandler.NewHttpError(fiber.StatusUnauthorized, "unauthorized", struct {
+			FirstName string
+			LastName  string
+		}{
+			FirstName: "John",
+			LastName:  "Wick",
+		}))
+	})
+
+	app.Listen(3000)
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/hendratommy/fiber-errhandler
+
+go 1.14
+
+require (
+	github.com/gofiber/fiber v1.10.1
+	github.com/gofiber/recover v0.1.0 // indirect
+	github.com/klauspost/compress v1.10.6 // indirect
+)
